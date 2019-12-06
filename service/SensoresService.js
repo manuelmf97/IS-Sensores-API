fa7ae20ad0261cf0163b7d205b8ea180e47ca4fe
manuelmf97 <57540091+manuelmf97@users.noreply.github.com>
2019-12-06 16:53:06 +0100

    Date.now()

--- service/SensoresService.js
+++ service/SensoresService.js
@@ -117,6 +117,7 @@ exports.getUbicacion = function(sensoresId) {
     var totalTime = time2 - time1;
     var socket = net.createConnection(2003, "carbon.hostedgraphite.com", function() {
     socket.write(apikey + ".sensores.ubicacion "+ String(totalTime) +"\n");
+      
     socket.end();
     });
     resolve(examples[Object.keys(examples)[0]]);
